2e23f0ea895c5170d0c657075f60acef19be23d5
Slavik Filipenko <43026442+Viacheslav-Filipenko@users.noreply.github.com>
2020-06-16 00:52:03 +0300

    fix calculation

--- src/app/core/services/integral-solver.service.ts
+++ src/app/core/services/integral-solver.service.ts
@@ -12,8 +12,8 @@ import { withAccuracy } from '../utils/with-accuracy.util';
 export class IntegralSolverService {
     public calculateByRectangles(options: ICalculateIntegralOptions): ICalculateIntegralResult {
         switch (options.method) {
-            // case RECTANGLES_METHODS.center:
-            //     return this.calculateByCenterRectangles(options);
+            case RECTANGLES_METHODS.center:
+                return this.calculateByCenterRectangles(options);
             case RECTANGLES_METHODS.rigth:
                 return this.calculateByRightRectangles(options);
             default:
@@ -34,7 +34,7 @@ export class IntegralSolverService {
             table.push({ x, result: functionResultWithRoundingAccuracy });
         }
 
-        const sumWithRoundingAccuracy = withAccuracy(sum, roundingAccuracy);
+        const sumWithRoundingAccuracy = withAccuracy(mathjs.multiply(sum, step), roundingAccuracy);
 
         return { sum: sumWithRoundingAccuracy, equalution, from, to, roundingAccuracy, step, table, intervals };
     }
@@ -53,12 +53,29 @@ export class IntegralSolverService {
             table.push({ x, result: functionResultWithRoundingAccuracy });
         }
 
-        const sumWithRoundingAccuracy = withAccuracy(sum, roundingAccuracy);
+        const sumWithRoundingAccuracy = withAccuracy(mathjs.multiply(sum, step), roundingAccuracy);
 
         return { sum: sumWithRoundingAccuracy, equalution, from, to, roundingAccuracy, step, table, intervals };
     }
 
-    // public calculateByCenterRectangles(options: ICalculateIntegralOptions): ICalculateIntegralResult {
-    //     let sum = 0;
-    // }
+    public calculateByCenterRectangles({ to, from, intervals, roundingAccuracy, equalution }: ICalculateIntegralOptions): ICalculateIntegralResult {
+        let sum = 0;
+        const table = [];
+        const step = mathjs.divide(mathjs.subtract(to, from), intervals) as number;
+        let previousValue = from;
+
+        for (let x = withAccuracy((mathjs.add(from, step) as number), roundingAccuracy); x <= to; x = withAccuracy((mathjs.add(x, step) as number), roundingAccuracy)) {
+            const functionResult = mathjs.evaluate(equalution, { x: mathjs.divide(mathjs.add(x, previousValue), 2) });
+            const functionResultWithRoundingAccuracy = withAccuracy(functionResult, roundingAccuracy);
+
+            sum = mathjs.add(sum, functionResultWithRoundingAccuracy) as number;
+            table.push({ x, result: functionResultWithRoundingAccuracy });
+
+            previousValue = x;
+        }
+
+        const sumWithRoundingAccuracy = withAccuracy(mathjs.multiply(sum, step), roundingAccuracy);
+
+        return { sum: sumWithRoundingAccuracy, equalution, from, to, roundingAccuracy, step, table, intervals };
+    }
 }
